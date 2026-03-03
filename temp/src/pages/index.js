95f41abeb5948912fd6b0f4f86b185dde5197f44
Elijah Gamson <egamson50@gmail.com>
2026-03-02 23:37:28 -0500

    Updated index.js

--- temp/src/pages/index.js
+++ temp/src/pages/index.js
@@ -1,14 +1,15 @@
-import Card from '../styles/cardStyles'; // ./ (look in same folder), ../ (go up a folder)
+import {LargeCard} from '../styles/cardStyles'; // ./ (look in same folder), ../ (go up a folder)
 import GlobalStyles from '../styles/GlobalStyles';
 import Nav from '../components/Nav';
+import QuoteBox from '../components/Quote'; //Quote API
 
 export default function HomePage(){
     return (
     <div id="background_image">
         <GlobalStyles/>
         <Nav></Nav>
-            <h2 id="title_text">Why Use NextRound?</h2>
-            <Card style={{width: '100%'}}>
+            <h2 style={{textAlign: 'center', padding: '10px', marginBottom: '15px'}}>Why Use NextRound?</h2>
+            <LargeCard>
             <ul>
                 <li>Find Jobs</li>
                 <li>Track applications</li>
@@ -16,7 +17,9 @@ export default function HomePage(){
                 <li>Keep all the information in one place</li>
                 <li>Get to the "NextRound" of interviews</li>
             </ul>
-        </Card>
+            <h3 style={{paddingTop: '10px'}}>Today's motivational quote to get you through the current state of the job market is:</h3>
+            <QuoteBox/>
+            </LargeCard>
     </div>
     );
 }
